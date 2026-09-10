15f5738eae4e6fe9ba0b9eaf72ca007059991ce2
94v1n <gavinkoch@seattleacademy.org>
2026-09-10 16:07:26 +0000

    Test

---
+++ about_me.py
@@ -0,0 +1,26 @@
+#I was bored. So i made this.
+# x = 5
+# if x == 5:
+#     print("True")
+# else:
+#     print("False")
+
+# An homage to UNDERTALE
+print("Howdy! My Name is Flowey!")
+print("Flowey the Flower!")
+fallen_human = input("What's you name? ")
+print (f"Well Howdy {fallen_human}!")
+# Optional Code, for more to UNDERTALE fun
+# print(f"{fallen_human}, in this world, we help each other with little 'friendliness pellets'")
+# print("Alright now. Get those pellets!")
+# input()
+# print("Maybe you didn't here me.")
+# print("Get those pellets")
+# input()
+# print("What are you doing?")
+# print("RUN INTO THE BULLETS!")
+# input()
+# print("In this world")
+# print("it's kill,")
+# print("or")
+# print("be killed!")
