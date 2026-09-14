e8db6494401a8a01ba6b626090520f95ef2b414d
94v1n <gavinkoch@seattleacademy.org>
2026-09-14 21:31:03 +0000

    Hey josh!

--- about_me.py
+++ about_me.py
@@ -1,16 +1,16 @@
-#I was bored. So i made this.
-# x = 5
+# #I was bored. So i made this.
+# x = 17
 # if x == 5:
 #     print("True")
 # else:
 #     print("False")
 
 # An homage to UNDERTALE
-print("Howdy! My Name is Flowey!")
-print("Flowey the Flower!")
-fallen_human = input("What's you name? ")
-print (f"Well Howdy {fallen_human}!")
-# Optional Code, for more to UNDERTALE fun
+# print("Howdy! My Name is Flowey!")
+# print("Flowey the Flower!")
+# fallen_human = input("What's you name? ")
+# print (f"Well Howdy {fallen_human}!")
+# # Optional Code, for more to UNDERTALE fun
 # print(f"{fallen_human}, in this world, we help each other with little 'friendliness pellets'")
 # print("Alright now. Get those pellets!")
 # input()
---
+++ project1.py
@@ -0,0 +1,4 @@
+print("Hello! This is a madlib. You will be asked to give word types, and then when all of the blanks are filled, then you will read the entire madlib. Have fun!")
+print()
+country = input("Alright! So first we need a country!" )
+print(country)
